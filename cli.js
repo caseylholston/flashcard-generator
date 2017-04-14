1d7869b25804eebd4bc986e6db9acfb1095357b6
caseylholston <caseylholston@gmail.com>
2017-04-14 14:41:46 -0400

    I have a working front end to take in data and push into log files -- Need work on outputting log files for quiz

--- cli.js
+++ cli.js
@@ -2,40 +2,103 @@
 var inquirer = require('inquirer');
 var fs = require('fs');
 var command = process.argv[2];
-var fullText = process.argv[3];
-var answer = process.argv[4];
+var fullText = '';
+var answer = '';
 
-function FlashCard (command, fullText, answer){
-     if (!(this instanceof FlashCard))
-        return new Flashcard(command, fullText, answer)
-    this.command = command;
-    this.fullText = fullText;
-    this.answer = answer;
-    this.arguments = Array.prototype.slice.call(arguments);
+
+var question1 =    [
+    {
+        type:'list',
+        name:'action',
+        message:'What would you like to do?',
+        choices:['Create a Flashcard', 'Study']
+    }];
+
+var question2a =    [
+    {
+        type:'list',
+        name:'createCardType',
+        message:'What type of card would you like to create?',
+        choices:['Basic', 'Cloze']
+    }];
+
+var question2b =    [
+    {
+        type:'list',
+        name:'quizType',
+        message:'What type of quiz would you like to take?',
+        choices:['Basic', 'Cloze']
+    }];
+
+
+var question3a =    [
+    {
+        type:'input',
+        name:'basicQuestion',
+        message:'Please type the question to be answered and press ENTER',
+    },
+    {
+        type:'input',
+        name:'basicAnswer',
+        message:'Please type the answer to the question and press ENTER',
+    }
+
+    ];
+
+var question3b =    [
+  {
+        type:'input',
+        name:'clozeQuestion',
+        message:'Please type the sentence to be used and press ENTER',
+    },
+    {
+        type:'input',
+        name:'cloze',
+        message:'Please type the cloze portion of the sentence and press ENTER',
+    }
+
+    ];
+
+// console.log(input);
+// console.log('Command: ' + input.command);
+// console.log('FullText: ' + input.fullText);
+// console.log('Answer: ' + input.answer);
+
+inquirer.prompt(question1).then(function (answers){
+    switch(answers.action) {
+    case 'Create a Flashcard':
+           inquirer.prompt(question2a).then(function (answers){
+                switch(answers.createCardType) {
+                case 'Basic':
+                    inquirer.prompt(question3a).then(function (answers){
+                    fs.appendFile('basic.txt', ','+ answers.basicQuestion +','+ answers.basicAnswer, function(err){
+                        if (err) (console.log('Error: ' + err))});
+                    });
+                break;
+
+                case 'Cloze':
+                    inquirer.prompt(question3b).then(function (answers){
+                    fs.appendFile('cloze.txt', ','+ answers.clozeQuestion +','+ answers.cloze, function(err){
+                        if (err) (console.log('Error: ' + err))});
+                    });
+                break;
+                } 
+            })
+    break;
+    
+    case 'Study':
+           inquirer.prompt(question2b).then(function (answers){
+                switch(answers.createCardType) {
+                case 'Basic':
+                doBasicQuiz();
+                break;
+
+                case 'Cloze':
+                doClozeQuiz();
+                break;
+                } 
+            })
+    break;    
+
 }
-
-
-var input = new FlashCard (command, fullText, answer);
-
-module.exports.FlashCard;
-
-console.log(input);
-console.log('Command: ' + input.command);
-console.log('FullText: ' + input.fullText);
-console.log('Answer: ' + input.answer);
-
-
-
-
-
-// switch(command) {
-// case 'basic':
-// input.newBasic();
-// // search.logIt();
-// break;
-// }
-
-// case 'cloze':
-// search.doAdmin();
-// break;
-// }
+});
